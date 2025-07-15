6b1a0454f25a6a6b7c01fc31c87d668a0257a912
Quan Huynh <163743120+hmquan191@users.noreply.github.com>
2025-07-15 08:57:07 +0700

    thử nghiệm đẩy lên vercel

--- FrontEnd/app/checkout/success/page.tsx
+++ FrontEnd/app/checkout/success/page.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client'
 
 import { useState, useEffect } from 'react'
@@ -18,7 +17,7 @@ export default function CheckoutSuccess() {
   const orderId = searchParams.get('orderId') || searchParams.get('orderid')
   const resultCode = searchParams.get('resultCode') || searchParams.get('resultcode')
   const message = searchParams.get('message') || searchParams.get('errorMessage')
-  const extraData = searchParams.get('extraData')
+  // const extraData = searchParams.get('extraData')
 
   useEffect(() => {
     const verifyPayment = async () => {
